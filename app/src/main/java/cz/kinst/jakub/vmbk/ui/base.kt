42c2cfb6716e630afd033fe8fd66c629c6f8b081
Jakub Kinst <jakub@kinst.cz>
2017-09-21 13:13:29 +0200

    BaseViewModel added

--- app/src/main/java/cz/kinst/jakub/vmbk/ui/base.kt
+++ app/src/main/java/cz/kinst/jakub/vmbk/ui/base.kt
@@ -1,24 +1,40 @@
 package cz.kinst.jakub.vmbk.ui
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModel
 import android.databinding.ViewDataBinding
+import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import cz.kinst.jakub.vmbk.R
+import cz.kinst.jakub.vmbk.SingleLiveData
 import cz.kinst.jakub.vmbk.ViewModelBinding
 import cz.kinst.jakub.vmbk.databinding.ActivityMainBinding
 import cz.kinst.jakub.vmbk.vmb
 
 
-// example of BaseActivity
-abstract class BaseActivity<VM : ViewModel, B : ViewDataBinding> : AppCompatActivity() {
+// example of Base classes
+abstract class BaseActivity<VM : BaseViewModel, B : ViewDataBinding> : AppCompatActivity() {
     abstract val vmb: ViewModelBinding<VM, B>
 
-    fun showSnackbar(message: String) {
-        Snackbar.make(vmb.rootView, message, Snackbar.LENGTH_SHORT).show()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        vmb.viewModel.displayMessage.observe(this, Observer {
+            Snackbar.make(vmb.rootView, it!!, Snackbar.LENGTH_SHORT).show()
+        })
     }
 }
 
-class MyGreatActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
-    override val vmb = vmb<MainViewModel, ActivityMainBinding>(R.layout.activity_main)
+abstract class BaseViewModel : ViewModel() {
+    val displayMessage = SingleLiveData<String>()
+}
+
+class MyGreatActivity : BaseActivity<MyGreatViewModel, ActivityMainBinding>() {
+    override val vmb = vmb<MyGreatViewModel, ActivityMainBinding>(R.layout.activity_main)
+}
+
+class MyGreatViewModel : BaseViewModel() {
+    fun showMessage() {
+        displayMessage.value = "Test Message!"
+    }
 }
